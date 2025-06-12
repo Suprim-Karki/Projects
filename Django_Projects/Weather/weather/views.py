65221bf5e0c94b0adb40a4f78a3db84d6c96a8bf
Suprim-Karki <shupreem1911@gmail.com>
2025-06-12 22:22:30 +0545

    Necessary imports for api

--- Django_Projects/Weather/weather/views.py
+++ Django_Projects/Weather/weather/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         city=request.POST['city']
     else:
-        city=''
+        city='Enter a City'
     return render(request,'index.html',{'city':city})
