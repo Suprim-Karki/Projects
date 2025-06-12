7458f844f64e9e15343da8398dcc77a23cc5c498
Suprim-Karki <shupreem1911@gmail.com>
2025-06-12 22:15:01 +0545

    Getting city name

--- Django_Projects/Weather/weather/views.py
+++ Django_Projects/Weather/weather/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        city=request.POST['city']
+    else:
+        city=''
+    return render(request,'index.html',{'city':city})
