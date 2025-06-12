a5b3d77275a64d565a637e09692019d40bdc9c4b
Suprim-Karki <shupreem1911@gmail.com>
2025-06-12 22:26:36 +0545

    Reading api

--- Django_Projects/Weather/weather/views.py
+++ Django_Projects/Weather/weather/views.py
@@ -6,6 +6,7 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city=request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+"&appid=7bb90ceb6c547e3c7aed7c71a5b743f1").read()
     else:
         city='Enter a City'
     return render(request,'index.html',{'city':city})
