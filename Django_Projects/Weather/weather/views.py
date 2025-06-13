b11cd68a043f37c62f6e5b22101f2154d771f589
Suprim-Karki <shupreem1911@gmail.com>
2025-06-13 15:32:55 +0545

    Weather app done

--- Django_Projects/Weather/weather/views.py
+++ Django_Projects/Weather/weather/views.py
@@ -6,7 +6,18 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city=request.POST['city']
-        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+"&appid=7bb90ceb6c547e3c7aed7c71a5b743f1").read()
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=7bb90ceb6c547e3c7aed7c71a5b743f1'
+        res = urllib.request.urlopen(url).read()
+        json_data=json.loads(res)    
+        data={
+            'country_code':str(json_data['sys']['country']),
+            'coordinate':str(json_data['coord']['lon'])+' '+str(json_data['coord']['lat']),
+            'temp': f"{json_data['main']['temp'] - 273:.2f}°C",
+            'pressure':f"{str(json_data['main']['pressure'])} hPa",
+            'humidity':f"{str(json_data['main']['humidity'])}%"
+        }
+        print(json_data)
     else:
         city='Enter a City'
-    return render(request,'index.html',{'city':city})
+        data=''
+    return render(request,'index.html',{'data':data,'city':city})
