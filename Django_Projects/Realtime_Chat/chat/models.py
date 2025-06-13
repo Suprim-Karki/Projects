a3329865e41e5dc41e5497538ab5028695a36a0c
Suprim-Karki <shupreem1911@gmail.com>
2025-06-13 22:28:53 +0545

    Message model

--- Django_Projects/Realtime_Chat/chat/models.py
+++ Django_Projects/Realtime_Chat/chat/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Room(models.Model):
     name=models.CharField(max_length=100)
+
+class Message(models.Model):
+    value=models.CharField(max_length=1000000)
+    date=models.CharField(default=datetime.now,blank=True)
+    user=models.CharField(max_length=1000000)
+    name=models.CharField(max_length=1000000)
