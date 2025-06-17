9b140851d6ad7514bf3dfd1de317fd389a30c50d
Suprim-Karki <shupreem1911@gmail.com>
2025-06-17 21:43:37 +0545

    Completed

--- Django_Projects/Realtime_Chat/chat/models.py
+++ Django_Projects/Realtime_Chat/chat/models.py
@@ -3,10 +3,9 @@ from datetime import datetime
 
 # Create your models here.
 class Room(models.Model):
-    name=models.CharField(max_length=100)
-
+    name = models.CharField(max_length=1000)
 class Message(models.Model):
-    value=models.CharField(max_length=1000000)
-    date=models.CharField(default=datetime.now,blank=True)
-    user=models.CharField(max_length=1000000)
-    name=models.CharField(max_length=1000000)
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000000)
+    room = models.CharField(max_length=1000000)
--- Django_Projects/Realtime_Chat/chat/views.py
+++ Django_Projects/Realtime_Chat/chat/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from chat.models import Room,Message
+from chat.models import Room, Message
 from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
 
 def room(request, room):
     username = request.GET.get('username')
@@ -25,7 +25,7 @@ def checkview(request):
         new_room = Room.objects.create(name=room)
         new_room.save()
         return redirect('/'+room+'/?username='+username)
-    
+
 def send(request):
     message = request.POST['message']
     username = request.POST['username']
