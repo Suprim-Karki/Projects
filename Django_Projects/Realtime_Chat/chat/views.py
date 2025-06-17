2545a0b4f6ed8e824daeb3163b6cc46003acc6d3
Suprim-Karki <shupreem1911@gmail.com>
2025-06-17 21:14:03 +0545

    Checkview functionality

--- Django_Projects/Realtime_Chat/chat/views.py
+++ Django_Projects/Realtime_Chat/chat/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from chat.models import Room,Message
 
 # Create your views here.
 def home(request):
@@ -8,4 +9,12 @@ def room(request,room):
     return render(request,'room.html')
 
 def checkview(request):
-    pass
+    room = request.POST['roomname']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
