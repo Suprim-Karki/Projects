ad7728d59bbaa02d4e81308666c4d95a47126526
Suprim-Karki <shupreem1911@gmail.com>
2025-07-07 21:12:49 +0545

    ContactForm class and name var

--- Django_Projects/Portfolio/portfolio/forms.py
+++ Django_Projects/Portfolio/portfolio/forms.py
@@ -1,3 +1,12 @@
 from django import forms
 from django.core.validators import EmailValidator
 from .models import Project
+
+class ContactForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Your Name'
+        })
+    )
