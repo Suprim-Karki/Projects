98e4446e2bf5f3531101aa1c3c0a696e5aa7869d
Suprim-Karki <shupreem1911@gmail.com>
2025-07-07 21:13:37 +0545

    subject var

--- Django_Projects/Portfolio/portfolio/forms.py
+++ Django_Projects/Portfolio/portfolio/forms.py
@@ -18,3 +18,11 @@ class ContactForm(forms.Form):
             'placeholder': 'Your Email'
         })
     )
+
+    subject = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Subject'
+        })
+    )
