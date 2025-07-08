c76aef7912d41fac668b637788b3e43150e07ce8
Suprim-Karki <shupreem1911@gmail.com>
2025-07-08 21:16:05 +0545

    url patterns

--- Django_Projects/Portfolio/portfolio/urls.py
+++ Django_Projects/Portfolio/portfolio/urls.py
@@ -4,3 +4,11 @@ from .views import (
     ProjectsView, ResumeView, 
     contact_view
 )
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('projects/', ProjectsView.as_view(), name='projects'),
+    path('resume/', ResumeView.as_view(), name='resume'),
+    path('contact/', contact_view, name='contact'),
+]
