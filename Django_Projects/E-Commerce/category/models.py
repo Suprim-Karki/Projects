bbfa4afd54097cd629b2552b2abf66331612af7f
Suprim-Karki <shupreem1911@gmail.com>
2025-07-13 17:08:51 +0545

    category class in models

--- Django_Projects/E-Commerce/category/models.py
+++ Django_Projects/E-Commerce/category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=100,unique=True)     #URL
+    description = models.TextField(max_length=255,blank=True)
+    cat_image = models.ImageField(upload='photos/categories',blank=True)    #upload to or upload?
+
+    def __str__(self):
+        return self.category_name
+
+
+
+
+
+
